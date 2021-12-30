1814996d30dbc387f175107379ddee21ab3c83a3
Ed <edward.syasin@gmail.com>
2021-12-31 02:40:14 +0300

    Настроить сборку

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+	<div class="main">
+		{{ hello }}
+	</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import Component from "vue-class-component";
+
+@Component
+export default class App extends Vue {
+	hello: string = "hello world";
+}
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {VueLoaderPlugin} = require('vue-loader');
+
+module.exports = {
+    entry: "/src/main.ts",
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: 'index.js'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: __dirname + "/public/index.html",
+            inject: "body"
+        }),
+        new VueLoaderPlugin()
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                use: 'vue-loader'
+            },
+            {
+                test: /\.tsx?$/,
+                loader: 'ts-loader',
+                exclude: /node_modules/,
+                options: {
+                    appendTsSuffixTo: [/\.vue$/]
+                }
+            },
+            {
+                test: /\.js$/,
+                use: 'babel-loader'
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    'vue-style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }
+        ]
+    },
+    resolve: {
+        extensions: [".js", ".vue", ".json"]
+    },
+    devServer: {
+        port: 3000
+    }
+}
